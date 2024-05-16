17193c27d0c43200eab4af4ae0bb1de09d2007f3
tarciosilva <tarciopereirasilva@outlook.com>
2024-05-15 22:09:40 -0300

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
+ 
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
 const mode = document.querySelector('body');
 const switcher = document.getElementById('flexSwitchCheckDefault');
 const navbarBackGround = document.querySelector('[data-navbar]');
