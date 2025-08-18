d55b53dc9de9c806653083fcecaccbbae2c9acf8
tarciosilva <tarciopereirasilva@outlook.com>
2025-08-18 11:49:18 -0300

    start new version

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const hamburgerButton = document.getElementById("hamburger-button");
+const offCanva = document.getElementById("offcanva");
+
+hamburgerButton.addEventListener("click", () => {
+  const isActive = hamburgerButton.classList.contains("is-active");
+
+  if (isActive) {
+    hamburgerButton.classList.remove("is-active");
+    offCanva.style.left = "-120%";
+  } else {
+    hamburgerButton.classList.add("is-active");
+    offCanva.style.left = "0";
+  }
+});
